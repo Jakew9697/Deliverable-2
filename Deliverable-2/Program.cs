a3a176c1234ec8d2347224e7ad6a29ea5452f1e0
Jakew9697 <62920821+Jakew9697@users.noreply.github.com>
2020-04-07 18:43:52 -0500

    Add files via upload

---
+++ Deliverable-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Deliverable_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string message = "";
+            int checksum = 0;
+            int asciiCode;
+            
+
+
+                Console.WriteLine( "What is your message?" );
+                input = Console.ReadLine().ToUpper(); //1 //2 //3
+            
+                foreach (char letter in input) //4 //5
+
+                {
+                asciiCode = (int)letter - 64; //6 //7
+                    message += asciiCode + "-"; //8
+                checksum = checksum + (int)letter;
+                   
+                }  
+            
+            message = message.Substring(0, message.Length - 1); //5
+            Console.WriteLine("Your encoded message is " + message );//10
+            Console.WriteLine("Message checksum is " + checksum);//10
+
+
+
+        }
+    }
+}
